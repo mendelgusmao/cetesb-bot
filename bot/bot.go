f6300b160993075eba47d8f49b9f0f73708e1e7c
MendelGusmao <mendelsongusmao@gmail.com>
2023-09-28 19:10:52 -0300

    implement basic bot behavior

--- bot/bot.go
+++ bot/bot.go
@@ -1,9 +1,58 @@
 package bot
 
 import (
-	"github.com/mendelgusmao/scoredb/lib/database"
+	"fmt"
+	"log"
+	"strings"
+
+	telegram "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"github.com/mendelgusmao/cetesb-telegram-bot/store"
 )
 
-func New(database *database.Database) *Bot {
-	return &Bot{database: database}
+func New(store *store.Store, telegramAPIToken string) *Bot {
+	telegram, err := telegram.NewBotAPI(telegramAPIToken)
+
+	if err != nil {
+		log.Panic(err)
+	}
+
+	return &Bot{store: store, telegram: telegram}
+}
+
+func (b *Bot) Work() {
+	go func() {
+		u := telegram.NewUpdate(0)
+		u.Timeout = 60
+
+		updates := b.telegram.GetUpdatesChan(u)
+
+		for update := range updates {
+			if update.Message != nil {
+				log.Printf("@%s: %s", update.Message.From.UserName, update.Message.Text)
+
+				result, err := b.store.Query(update.Message.Text)
+
+				if err != nil {
+					log.Printf("[ERROR] @%s: %s => %v", update.Message.From.UserName, update.Message.Text, err)
+					msg := telegram.NewMessage(update.Message.Chat.ID, unknownErrorMessage)
+					b.telegram.Send(msg)
+				}
+
+				lines := make([]string, len(result.Beaches))
+
+				for index, result := range result.Beaches {
+					lines[index] = fmt.Sprintf(
+						"%s A praia %s na cidade de %s está %s para banho!",
+						ProperEmojiMapping[result.Proper],
+						strings.Title(strings.ToLower(result.Name)),
+						strings.Title(result.City.Name),
+						ProperTextMapping[result.Proper],
+					)
+				}
+
+				msg := telegram.NewMessage(update.Message.Chat.ID, strings.Join(lines, "\n"))
+				b.telegram.Send(msg)
+			}
+		}
+	}()
 }
--- bot/types.go
+++ bot/types.go
@@ -1,9 +1,24 @@
 package bot
 
 import (
-	"github.com/mendelgusmao/scoredb/lib/database"
+	telegram "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"github.com/mendelgusmao/cetesb-telegram-bot/store"
+)
+
+const unknownErrorMessage = "Ocorreu um erro na consulta. Tente novamente mais tarde."
+
+var (
+	ProperEmojiMapping = map[bool]string{
+		true:  "\U0001F7E2",
+		false: "\U0001F534",
+	}
+	ProperTextMapping = map[bool]string{
+		true:  "própria",
+		false: "imprópria",
+	}
 )
 
 type Bot struct {
-	database *database.Database
+	store    *store.Store
+	telegram *telegram.BotAPI
 }
