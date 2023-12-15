dc1fc05a2c535a3bf7770c385f55478889a86c29
MendelGusmao <mendelsongusmao@gmail.com>
2023-12-15 17:39:18 -0300

    Revert "implement store.CreateOrUpdateCollection"
    This reverts commit 4d2ee3b5911b789a84a1de546a93ceef9cc20ef6.

--- store/store.go
+++ store/store.go
@@ -17,22 +17,14 @@ func New(database *database.Database, scraper *scraper.Scraper) *Store {
 	}
 }
 
-func (s *Store) CreateOrUpdateCollection(collectionName string, config database.Configuration, documents []database.Document) error {
-	if s.database.CollectionExists(collectionName) {
-		return s.database.UpdateCollection(collectionName, documents)
-	}
-
-	return s.database.CreateCollection(collectionName, config, documents)
-}
-
 func (s *Store) ScrapeAndStore() error {
 	cities, beaches := s.ScrapeAndTransform()
 
-	if err := s.CreateOrUpdateCollection("cities", databaseConfiguration, cities); err != nil {
+	if err := s.Store("cities", cities); err != nil {
 		return err
 	}
 
-	return s.CreateOrUpdateCollection("beaches", databaseConfiguration, beaches)
+	return s.Store("beaches", beaches)
 }
 
 func (s *Store) ScrapeAndTransform() (cities []database.Document, beaches []database.Document) {
